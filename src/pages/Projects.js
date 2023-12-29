64e269d83135cd1482d3118200ebc59a7aae555b
Nathan Peters <peters.nate8@gmail.com>
2023-12-29 13:13:16 -0700

    added links for each project component instead of all displayed on page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,17 +4,43 @@ import JSProjects from './JSProjects';
 import GameProjects from './GameProjects';
 import DataSciProjects from './DataSciProjects';
 
+import { Link, Routes, Route } from 'react-router-dom';
+
 const Projects = () => {
   return (
     <>
       <h2 className='text-light fs-2 fw-bold d-flex justify-content-md-center'>Projects</h2>
       <p className='small fst-italic text-center'>(click on image to go to URL or GitHub repo)</p>
       <hr className='mb-5 pb-4' />
-      <ReactProjects />
-      <WebDevProjects />
+      <div className='btn-group d-flex justify-content-center gap-2 mb-5'>
+        <Link to='/' className='btn btn-sm text-nowrap overflow-hidden btn-light rounded active'>
+          React
+        </Link>
+        <Link to='/webdev' className='btn btn-sm text-nowrap overflow-hidden btn-light rounded'>
+          Web Dev
+        </Link>
+        <Link to='/javascript' className='btn btn-sm text-nowrap overflow-hidden btn-light rounded'>
+          JavaScript
+        </Link>
+        <Link to='/gamedev' className='btn btn-sm text-nowrap overflow-hidden btn-light rounded'>
+          Game Dev
+        </Link>
+        <Link to='/datascience' className='btn btn-sm text-nowrap overflow-hidden btn-light rounded'>
+          Data Science
+        </Link>
+      </div>
+      <Routes>
+        <Route path='/' element={<ReactProjects />} />
+        <Route path='/webdev' element={<WebDevProjects />} />
+        <Route path='/javascript' element={<JSProjects />} />
+        <Route path='/gamedev' element={<GameProjects />} />
+        <Route path='/datascience' element={<DataSciProjects />} />
+      </Routes>
+      
+      {/* <WebDevProjects />
       <JSProjects />
       <GameProjects />
-      <DataSciProjects />
+      <DataSciProjects /> */}
     </>
   );
 }
